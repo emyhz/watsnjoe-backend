a3988fca1d37d6a05489022c764ba770d05e301a
Heinz,Emily E.F. <e.heinz@student.fontys.nl>
2026-03-11 14:23:03 +0100

    Initial project structure

---
+++ src/main/java/nl/watsnjoe/s5/watsnjoebackend/WatsnjoeBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.watsnjoe.s5.watsnjoebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatsnjoeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatsnjoeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/watsnjoe/s5/watsnjoebackend/WatsnjoeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.watsnjoe.s5.watsnjoebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatsnjoeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
